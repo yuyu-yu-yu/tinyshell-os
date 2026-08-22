ef5eda12b5cb946bc1a609cfe4d259b9ddb11286
Young Claude <fangluyang7@gmail.com>
2026-08-22 23:27:24 +0800

    C: x86: add PIC and IRQ dispatch

--- kernel/arch/x86/interrupt.c
+++ kernel/arch/x86/interrupt.c
@@ -1,9 +1,15 @@
 #include "arch/x86/interrupt.h"
 
+#include "arch/x86/irq.h"
 #include "console.h"
 
 void interrupt_dispatch(struct interrupt_frame *frame)
 {
+    if (frame->vector >= 32U && frame->vector < 48U) {
+        irq_dispatch((uint8_t)(frame->vector - 32U));
+        return;
+    }
+
     console_write("EXCEPTION vector=");
     console_write_u32_dec(frame->vector);
     console_putc('\n');
