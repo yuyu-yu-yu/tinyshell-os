de84b60e66d7375888401de07f75644ea8346171
yuyu-yu-yu <1171910922@qq.com>
2026-08-25 01:36:27 +0800

    mm: add i386 paging

--- kernel/arch/x86/interrupt.c
+++ kernel/arch/x86/interrupt.c
@@ -1,6 +1,7 @@
 #include "arch/x86/interrupt.h"
 
 #include "arch/x86/irq.h"
+#include "arch/x86/paging.h"
 #include "console.h"
 
 void interrupt_dispatch(struct interrupt_frame *frame)
@@ -8,6 +9,10 @@ void interrupt_dispatch(struct interrupt_frame *frame)
     if (frame->vector >= 32U && frame->vector < 48U) {
         irq_dispatch((uint8_t)(frame->vector - 32U));
         return;
+    }
+
+    if (frame->vector == 14U) {
+        paging_handle_page_fault(frame);
     }
 
     console_write("EXCEPTION vector=");
